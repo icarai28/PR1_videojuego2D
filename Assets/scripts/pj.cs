b937eef58de1deffa673e3ac3ef2210eae4c1903
icarai28 <africacruiz@gmail.com>
2026-03-25 13:32:10 +0100

    suma

--- Assets/scripts/pj.cs
+++ Assets/scripts/pj.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class pj: MonoBehaviour
 {
+
+int miNumero= 5;
+float miNumeroflotante=0.8f;
+string miCadenaDeTexto="holita, soy un texto";
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +19,16 @@ public class pj: MonoBehaviour
     void Update()
     {
         Debug.Log("hola");
+    }
+
+
+
+    int Sumar(int num1, int num2);
+    {
+        
+        int Suma=num1+num2;
+
+        return suma;
+
     }
 }
