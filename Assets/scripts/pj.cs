7361d7b9e181b91535b060a0fadfdc58936a1970
icarai28 <africacruiz@gmail.com>
2026-03-25 13:11:43 +0100

    inicio

---
+++ Assets/scripts/pj.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class pj : MonoBehaviour
+{
+    int 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("inicio");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("hola");
+    }
+}
