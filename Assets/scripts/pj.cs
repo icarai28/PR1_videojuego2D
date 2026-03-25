7f5eeee050374a6c0d7860e5e4e89bec3589d11c
icarai28 <africacruiz@gmail.com>
2026-03-25 14:18:00 +0100

    locatione

--- Assets/scripts/pj.cs
+++ Assets/scripts/pj.cs
@@ -7,7 +7,7 @@ public class pj: MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(this.transform)
+        Debug.Log(this.transform);
     }
 
     // Update is called once per frame
