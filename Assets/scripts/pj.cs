2c9f0218181707a0a1c7d0fd0f7186cdefb5e34a
icarai28 <africacruiz@gmail.com>
2026-03-25 14:27:15 +0100

    lock in

--- Assets/scripts/pj.cs
+++ Assets/scripts/pj.cs
@@ -7,13 +7,12 @@ public class pj: MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(this.transform);
+     public   Vector3 inicioPersonaje=new Vector3(1,1,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hola");
     }
 
 
