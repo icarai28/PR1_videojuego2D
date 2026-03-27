1862f5bfbb5d1dcb14633ad9956b01669e5805df
icarai28 <africacruiz@gmail.com>
2026-03-27 11:54:31 +0100

    Posicion

--- Assets/scripts/pj.cs
+++ Assets/scripts/pj.cs
@@ -7,22 +7,16 @@ public class pj: MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-     public   Vector3 inicioPersonaje=new Vector3(1,1,0);
+    this.transform.position=inicioPersonaje
+    
+     public Vector3 inicioPersonaje=new Vector3(1,1,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
 
-
-
-    int Sumar(int num1, int num2)
-    {
         
-        int Suma=num1+num2;
-
-        return Suma;
-
     }
+
 }
