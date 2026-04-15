776148c5ed1a078f8fd531e574074866190e3e80
icarai28 <africacruiz@gmail.com>
2026-04-15 14:26:57 +0200

    coin clase

--- Assets/scripts/pj.cs
+++ Assets/scripts/pj.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class pj: MonoBehaviour
 {
 
+     public Vector3 inicioPersonaje=new Vector3(1,1,0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    this.transform.position=inicioPersonaje
-    
-     public Vector3 inicioPersonaje=new Vector3(1,1,0);
-    }
+    this.transform.position = inicioPersonaje;
+        }
 
     // Update is called once per frame
     void Update()
     {
+    this.transform.position = new Vector3(this.transform.position.x+0.1f,this.transform.position.y,this.transform.position=inicioPersonaje.z);
+
 
         
     }
